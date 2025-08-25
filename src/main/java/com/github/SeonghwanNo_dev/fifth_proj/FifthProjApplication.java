16963065b4dda91d0b72a5ee7ddf75c821dab202
nanamamgaga <seonghwangood01@gmail.com>
2025-08-25 15:42:56 +0900

    feat: Re-upload initial project files due to corruption

---
+++ src/main/java/com/github/SeonghwanNo_dev/fifth_proj/FifthProjApplication.java
@@ -0,0 +1,13 @@
+package com.github.SeonghwanNo_dev.fifth_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifthProjApplication.class, args);
+	}
+
+}
